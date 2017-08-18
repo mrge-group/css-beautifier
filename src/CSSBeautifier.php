bdfa9462085929e97c385a23c734436ccd020035
Robert Braatz <webcodecs@gmail.com>
2017-08-18 14:22:48 +0200

    fixed codingstyle

--- src/CSSBeautifier.php
+++ src/CSSBeautifier.php
@@ -2,11 +2,11 @@
 
 namespace Shopping24\CSSBeautifier;
 
-
 class CSSBeautifier
 {
 
-    public static function run($string){
+    public static function run($string)
+    {
         $tap = "    ";
 
         $beautifulCssWithoutTaps = self::random($string);
@@ -15,55 +15,43 @@ class CSSBeautifier
 
         $tag = false;
         $media = false;
-        foreach(explode(PHP_EOL, $beautifulCssWithoutTaps) as $line){
-
-            if(preg_match('({)', $line)){
-
-                if($media === true) $beautifulCss .= $tap;
-                if(preg_match('(@)', $line)){
-
+        foreach (explode(PHP_EOL, $beautifulCssWithoutTaps) as $line) {
+            if (preg_match('({)', $line)) {
+                if ($media === true) {
+                    $beautifulCss .= $tap;
+                }
+                if (preg_match('(@)', $line)) {
                     $media = true;
-
-                }else{
-
+                } else {
                     $tag = true;
-
                 }
                 $beautifulCss .= $line."\n";
-
-            }elseif(preg_match('(})', $line)){
-
-                if($tag === true){
-
+            } elseif (preg_match('(})', $line)) {
+                if ($tag === true) {
                     $tag = false;
-                    if($media === true){
-
+                    if ($media === true) {
                         $beautifulCss .= $tap;
-
                     }
-
-                }else{
-
+                } else {
                     $media = false;
-
                 }
                 $beautifulCss .= $line."\n";
-
-            }else{
-
-                if($media === true) $beautifulCss .= $tap;
-                if($tag === true) $beautifulCss .= $tap;
+            } else {
+                if ($media === true) {
+                    $beautifulCss .= $tap;
+                }
+                if ($tag === true) {
+                    $beautifulCss .= $tap;
+                }
                 $beautifulCss .= $line."\n";
-
             }
-
         }
-        return $beautifulCss;
 
+        return $beautifulCss;
     }
 
-    private static function random($ugly){
+    private static function random($ugly)
+    {
         return preg_replace("(})", "\n$0", preg_replace("([{;}])", "$0\n", $ugly));
     }
-
 }
