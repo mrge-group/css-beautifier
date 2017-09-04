6121acbb5988ecfcf09b242c2f9115d8241bb3bf
Phil404 <philipp.96hh@hotmail.de>
2017-09-04 14:46:34 +0200

    [php] optimize the tap logic and add handling for more than one double point

--- src/CSSBeautifier.php
+++ src/CSSBeautifier.php
@@ -6,14 +6,13 @@ class CSSBeautifier
 {
     const TAP = "    ";
 
-    private static $media = false;
-    private static $tag = false;
     private static $repair = false;
 
     public static function run($string, bool $repair = true)
     {
         $start = microtime(true);
         self::$repair = $repair;
+        $taps = 0;
 
         $beautifiedArray = [];
 
@@ -21,38 +20,37 @@ class CSSBeautifier
             $line = trim($line);
 
             switch (true) {
-                case preg_match('/@/', $line):
-                    $line = self::checkHealthyWhiteSpaces($line);
-                    self::$media = true;
+                case preg_match('/(@supports)/', $line):
+                case preg_match('/(@media)/', $line):
+                    $line = self::checkHealthyWhiteSpaces(self::createTaps($line, $taps));
+                    $taps++;
                     break;
                 case preg_match('/{/', $line):
-                    $line = self::checkHealthyWhiteSpaces(self::createTaps($line), false);
-                    self::$tag = true;
+                    $line = self::checkHealthyWhiteSpaces(self::createTaps($line, $taps), false);
+                    $taps++;
                     break;
                 case preg_match('/}/', $line):
-                    self::$tag ? self::$tag = false : self::$media = false;
-                    $line = self::createTaps($line);
+                    $taps--;
+                    $line = self::createTaps($line, $taps);
                     break;
                 default:
-                    $line = self::checkHealthyWhiteSpaces(self::createTaps($line));
+                    $line = self::checkHealthyWhiteSpaces(self::createTaps($line, $taps));
             }
 
-            //$line = !preg_match('/@/', $line) ? $line = self::createTaps($line) : $line;
             $beautifiedArray[$key] = self::$repair ? self::checkHealthyAttribute($line) : $line;
         }
 
         $s = self::arrayToString($beautifiedArray);
-
 
         var_dump((microtime(true) - $start) * 1000);
         return $s;
     }
 
 
-    /*
+    /**
      * This function will add "new Lines" after every "{;}".
      *
-     * @param String    $string
+     * @param String $string
      *
      * @return String
      */
@@ -61,26 +59,26 @@ class CSSBeautifier
         return preg_replace("/}/", "\n$0", preg_replace("/[{;}]/", "$0\n", $string));
     }
 
-    /*
+    /**
      * This function will convert a string to an array.
      * Each line will be an item in the array.
      *
-     * @param String    $string     The string to convert
+     * @param String $string
      *
-     * @return Array
+     * @return array
      */
     private static function stringToArray($string)
     {
         return explode(PHP_EOL, self::newLines($string));
     }
 
-    /*
+    /**
      * This function convert an array to a string.
      * Each item in the array is a new line in the string.
      *
-     * @param array $array The array to convert
+     * @param array $array
      *
-     * @return string $string The converted string
+     * @return string
      */
     private static function arrayToString(array $array)
     {
@@ -96,8 +94,12 @@ class CSSBeautifier
         return $string;
     }
 
-    /*
+    /**
+     * This function will check the ';' in the end of a line and will add one if needed.
      *
+     * @param string $string
+     *
+     * @return string
      */
     private static function checkHealthyAttribute(string $string)
     {
@@ -107,10 +109,22 @@ class CSSBeautifier
         return $string;
     }
 
+    /**
+     * This function will check the line for a healthy structure.
+     *
+     * @param string $string
+     * @param bool $checkDoublePoints
+     *
+     * @return string
+     */
     private static function checkHealthyWhiteSpaces(string $string, bool $checkDoublePoints = true)
     {
-        if (!preg_match('/: /', $string) && $checkDoublePoints) {
-            $string = preg_replace('/:/', ': ', $string);
+        if (preg_match('/:/', $string) && !preg_match('/: /', $string) && $checkDoublePoints) {
+            $string = preg_replace('/:/', ': ', $string, 1);
+            if (preg_match('/(url\()/', $string)) {
+                preg_match('~(url\()(.*?)\)~', $string, $result);
+                $string = str_replace($result[0], preg_replace('/ /', '', $result[0]), $string);
+            }
         }
         if (!preg_match('/ {/', $string)) {
             $string = preg_replace('/{/', ' {', $string);
@@ -118,15 +132,20 @@ class CSSBeautifier
         return $string;
     }
 
-    /*
+    /**
+     * This function will create the needed taps.
      *
+     * @param string $string
+     * @param int $taps
+     *
+     * @return string
      */
-    private static function createTaps(string $string)
+    private static function createTaps(string $string, int $taps)
     {
         $string = str_replace(',', ', ', $string);
-        $string = self::$tag ? self::TAP . $string : $string;
-        $string = self::$media ? self::TAP . $string : $string;
-
+        for ($i = 0; $i < $taps; $i++) {
+            $string = self::TAP . $string;
+        }
         return $string;
     }
 }
