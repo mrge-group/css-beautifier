fcab06f2fc3c1c63e58e59b21aa40418de0d3a7c
Robert Braatz <webcodecs@gmail.com>
2017-09-07 10:53:35 +0200

    add test for three intend case

--- tests/CSSBeautifierTest.php
+++ tests/CSSBeautifierTest.php
@@ -8,7 +8,7 @@ final class CSSBeautifierTest extends TestCase
     /**
      * @test
      */
-    public function testThatTagsCanBeBeautify()
+    public function testThatTagsCanBeBeautified()
     {
         $ugly = "h1{font-size:99px;}";
         self::assertEquals("h1 {\n    font-size: 99px;\n}", CSSBeautifier::run($ugly));
@@ -17,7 +17,7 @@ final class CSSBeautifierTest extends TestCase
     /**
      * @test
      */
-    public function testThatMoreThanOneTagCanBeBeautify()
+    public function testThatMoreThanOneTagCanBeBeautified()
     {
         $ugly = "h1{font-size:99px;}h2{color:#123123;}";
         self::assertEquals("h1 {\n    font-size: 99px;\n}\nh2 {\n    color: #123123;\n}", CSSBeautifier::run($ugly));
@@ -48,5 +48,15 @@ final class CSSBeautifierTest extends TestCase
     {
         $ugly = file_get_contents('tests/files/ugly.css');
         self::assertEquals(file_get_contents('tests/files/beauty.css'), CSSBeautifier::run($ugly, false));
+    }
+
+    /**
+     * @test
+     */
+    public function testThreeIntendCase()
+    {
+        $ugly = '@asd{@media{h1{color:blue;}}}';
+        var_dump(CSSBeautifier::run($ugly, false));
+        self::assertEquals("@asd {\n    @media {\n        h1 {\n            color: blue;\n        }\n    }\n}", CSSBeautifier::run($ugly, false));
     }
 }
